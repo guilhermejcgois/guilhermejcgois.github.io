3e5486290d3431f4dad24be43fbf28153e700f79
Guilherme Gois <guilhermegois@guilherme.gois-Laptop>
2018-05-05 17:16:58 -0300

    Configures webpack alongside pug js template engine

---
+++ src/index.js
@@ -0,0 +1,1 @@
+document = require('./index.pug');
---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
+    },
+    module: {
+        rules: [{
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|ico)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
+            {
+                test: /\.pug$/,
+                use: [
+                    'pug-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({ template: './src/index.pug' })
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
