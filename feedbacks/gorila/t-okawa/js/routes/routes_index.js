cd2104cf292b141acb6f09c8bfd71e5a1a1872ea
Gois <guijocargo@gmail.com>
2018-07-24 12:03:57 -0300

    Add feedback for candidate T. Okawa

---
+++ feedbacks/gorila/t-okawa/js/routes/routes_index.js
@@ -0,0 +1,1 @@
+var ROUTES_INDEX = {"name":"<root>","kind":"module","className":"AppModule","children":[{"name":"appRoutes","filename":"src/app/app-routing.module.ts","module":"AppRoutingModule","children":[{"path":"home","component":"HomePageComponent"},{"path":"products-list","component":"ProductsListComponent"},{"path":"","redirectTo":"home","pathMatch":"full"}],"kind":"module"}]}
